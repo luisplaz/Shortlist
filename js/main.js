c95f50557f6202630380b1bcab1e9affb79c04c8
luisplaz <luisplaz@hotmail.com>
2016-04-09 20:31:11 -0400

    Add Olivia

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
 $(document).ready(function() {
-	
 	var counter = 0;
-	var questions = ["Hello?","Yeah?","Ok?"];
-	var text = "Hello my name is Gary, I will be your interviewer.\n Press start to ";
-	
-	$('#inputQ').attr("value",text);
+	var questions = [
+	"I can see you are a web developer. What type of language do you typically use?",
+	"You answered Ruby and Javascript, the can you please answer the next question:"+ "\n" +" What type of search method would you use for....",
+	"How would you implement a stack using ruby? [You can pseudocode]"];
+	var text = "Hello my name is Olivia, I will be your interviewer.\n Press start to begin!";
+	$('#answer-form').hide();
+
+	text.split("").forEach(function(elem, index){
+		setTimeout(function(){
+			$("#inputQ").val($("#inputQ").val()+elem);
+		}, index* 55); 
+		$("#inputQ").focus();
+	}); 
 
 	$("#start_button").click(function(e){
 		e.preventDefault();
-		counter = (counter + 1) % questions.length;
-		$('#inputQ').attr("value",questions[counter]);
-		$("#start_button").text("Submit")
+		$('#answer-form').show();
+		$('#inputP').val("");
+		if(counter!=(questions.length)){
+			debugger;
+			$('#inputQ').val(questions[counter]);
+			$("#start_button").text("Submit")
+			counter = counter + 1;
+		}else{
+			$('.jumbotron').hide();
+			$('.text-center').append("<br><br><h4>Thank you for your time</h4>");
+		}
 	});
 });
